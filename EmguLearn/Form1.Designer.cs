02c8fa6e1582530c44de148523f1ec78c2d06601
thosakwe <thosakwe@gmail.com>
2016-07-14 13:39:38 -0400

    Woohoo!

--- EmguLearn/Form1.Designer.cs
+++ EmguLearn/Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.preview = new System.Windows.Forms.PictureBox();
+            this.lblCursor = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(584, 362);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // ofd
+            // 
+            this.ofd.Filter = "Image Files|*.png;*.jpg;*.jpeg";
+            this.ofd.Title = "Choose an Image...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(433, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // preview
+            // 
+            this.preview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.preview.Location = new System.Drawing.Point(433, 260);
+            this.preview.Name = "preview";
+            this.preview.Size = new System.Drawing.Size(139, 90);
+            this.preview.TabIndex = 2;
+            this.preview.TabStop = false;
+            // 
+            // lblCursor
+            // 
+            this.lblCursor.AutoSize = true;
+            this.lblCursor.BackColor = System.Drawing.SystemColors.Control;
+            this.lblCursor.ForeColor = System.Drawing.Color.Red;
+            this.lblCursor.Location = new System.Drawing.Point(145, 83);
+            this.lblCursor.Name = "lblCursor";
+            this.lblCursor.Size = new System.Drawing.Size(53, 13);
+            this.lblCursor.TabIndex = 3;
+            this.lblCursor.Text = "CURSOR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.lblCursor);
+            this.Controls.Add(this.preview);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox preview;
+        private System.Windows.Forms.Label lblCursor;
     }
 }
 
--- EmguLearn/Form1.cs
+++ EmguLearn/Form1.cs
@@ -7,14 +7,167 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
 
 namespace EmguLearn
 {
     public partial class Form1 : Form
     {
+        CircleF cursor;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Bitmap processImage(Image<Bgr, byte> image)
+        {
+            var img = image.Resize(400, 400, Inter.Linear, true);
+
+            var uimg = new UMat();
+            CvInvoke.CvtColor(img, uimg, ColorConversion.Bgr2Gray);
+
+            var pyrDown = new UMat();
+            CvInvoke.PyrDown(uimg, pyrDown);
+            CvInvoke.PyrUp(pyrDown, uimg);
+
+            var circles = CvInvoke.HoughCircles(uimg, HoughType.Gradient, 2, 20, 180, 120, 5);
+            foreach (var circle in circles)
+            {
+                img.Draw(circle, new Bgr(Color.Pink), 3);
+            }
+
+            // Canny and edge detection
+            var cannyEdges = new UMat();
+            CvInvoke.Canny(uimg, cannyEdges, 180, 120);
+
+            var lines = CvInvoke.HoughLinesP(cannyEdges, 1, Math.PI / 45.0, 20, 30, 10);
+
+            using (var contours = new VectorOfVectorOfPoint())
+            {
+                CvInvoke.FindContours(cannyEdges, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+                int count = contours.Size;
+
+                for (int i = 0; i < count; i++)
+                {
+                    using (var contour = contours[i])
+                    {
+                        using (var approxContour = new VectorOfPoint())
+                        {
+                            CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * .05, true);
+                            // Only consider contours greater than 250 area
+                            if (CvInvoke.ContourArea(approxContour, false) > 250)
+                            {
+                                var points = approxContour.ToArray();
+
+                                if (approxContour.Size == 3)
+                                {
+                                    // Size tells how many vertices...
+                                    var triangle = new Triangle2DF(points[0], points[1], points[2]);
+                                    img.Draw(triangle, new Bgr(Color.Orange), 3);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return img.ToBitmap();
+        }
+
+        Bitmap processImage(string fileName)
+        {
+            return processImage(new Image<Bgr, byte>(fileName));
+        }
+
+        float makeCoord(float coord, int width)
+        {
+            var factor = coord / (width * 1.0F);
+            return factor * Width;
+        }
+
+        void runMouse(Mat mat)
+        {
+            var img = mat.ToImage<Bgr, byte>().Flip(FlipType.Horizontal).Resize(400, 400, Inter.Linear, true);
+
+            var uimg = new UMat();
+            CvInvoke.CvtColor(img, uimg, ColorConversion.Bgr2Gray);
+
+            var pyrDown = new UMat();
+            CvInvoke.PyrDown(uimg, pyrDown);
+            CvInvoke.PyrUp(pyrDown, uimg);
+
+            var circles = CvInvoke.HoughCircles(uimg, HoughType.Gradient, 2, 20, 180, 120, 5);
+
+            //var cursorImg = mat.ToImage<Bgr, byte>().CopyBlank();
+            //cursorImg.Mat.SetTo(new Bgr(Color.White).MCvScalar);
+
+            if (circles.Length > 0)
+            {
+                cursor = circles[0];
+                img.Draw(cursor, new Bgr(Color.Pink), 2);
+            }
+
+            /*if (cursor.Area > 0)
+            {
+                cursorImg.Draw(cursor, new Bgr(Color.Red), 2);
+            }*/
+
+            var x = makeCoord(cursor.Center.X - cursor.Radius, img.Width);
+            var y = makeCoord(cursor.Center.Y - cursor.Radius, img.Width);
+
+            lblCursor.Location = new Point(Convert.ToInt32(x), Convert.ToInt32(y));
+            //pictureBox1.BackgroundImage = cursorImg.ToBitmap();
+            preview.BackgroundImage = img.ToBitmap();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var capture = new Capture();
+            //var haar = new CascadeClassifier("..\\hand.xml");
+
+            Application.Idle += (_sender, _e) =>
+            {
+                //pictureBox1.BackgroundImage = processImage(capture.QueryFrame().ToImage<Bgr, byte>());
+                runMouse(capture.QueryFrame());
+
+                if (MakeRect(lblCursor).IntersectsWith(MakeRect(button1)))
+                {
+                    button1.Text = "ACTIVE";
+                }
+                else
+                {
+                    button1.Text = "INACTIVE";
+                }
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clicked!");
+        }
+
+        Rectangle MakeRect(Control control)
+        {
+            return new Rectangle(control.Location, control.Size);
+        }
+
+        void PaintControl(Pen pen, Control control, Graphics graphics)
+        {
+            graphics.DrawRectangle(pen, MakeRect(control));
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
         }
     }
 }
